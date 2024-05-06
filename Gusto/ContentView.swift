4a6ab38aa7bb821239a979433b297ddcf0c02c8f
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2024-05-06 15:11:58 -0700

    first commit

---
+++ Gusto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gusto
+//
+//  Created by Brandon Johns on 5/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gusto/GustoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GustoApp.swift
+//  Gusto
+//
+//  Created by Brandon Johns on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct GustoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
